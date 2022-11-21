f45ff6bc86911485ba3732945094da94b6e65086
RäDev <github@rayraydev.dev>
2022-11-21 16:52:25 -0700

    Added pages for Hamburger Menu items.

--- final_project/lib/main.dart
+++ final_project/lib/main.dart
@@ -2,7 +2,9 @@
 import 'package:flutter/material.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
 
-void main() => runApp(MainPageSecuritySystemApp());
+void main() => runApp(MaterialApp(
+      home: MainPageSecuritySystemApp(),
+    ));
 
 class MainPageSecuritySystemApp extends StatefulWidget {
   @override
@@ -23,9 +25,9 @@ class _SecuritySystemAppState extends State<MainPageSecuritySystemApp> {
                   TypewriterAnimatedText(
                     "Welcome!",
                     textStyle: const TextStyle(
-                      fontSize: 48,
-                      fontWeight: FontWeight.bold,
-                    ),
+                        fontSize: 48,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "Horizon"),
                     speed: const Duration(milliseconds: 250),
                   ),
                 ],
@@ -52,19 +54,27 @@ class _SecuritySystemAppState extends State<MainPageSecuritySystemApp> {
                       Icons.home,
                     ),
                     title: const Text("Home"),
-                    onTap: (() => Navigator.pop(context)),
+                    onTap: (() => Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => HomeScreen()))),
                   ),
                   ListTile(
-                    leading: const Icon(
-                      Icons.alarm,
-                    ),
-                    title: const Text("Alarms"),
-                    onTap: () => Navigator.pop(context),
-                  ),
+                      leading: const Icon(
+                        Icons.alarm,
+                      ),
+                      title: const Text("Alarms"),
+                      onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => AlarmsScreen(),
+                          ))),
                   ListTile(
                     leading: const Icon(Icons.settings),
                     title: const Text("Settings"),
-                    onTap: (() => Navigator.pop(context)),
+                    onTap: (() => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SettingsScreen(),
+                        ))),
                   )
                 ],
               ),
@@ -83,6 +93,52 @@ class _SecuritySystemAppState extends State<MainPageSecuritySystemApp> {
           ),
         ),
       ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Text(
+            "Welcome to my Security app!\nIn this app, you'll be able to monitor various sensors while outside the house, allowing you to ensure your family and belongings stay safe!\nAs this is more akin to a proof-of-concept, you'll have to manually activate the app's alarms, but it is a good illustration as to what one could ezpect when the app is fully completed!\nI thank you for your consideration of this app, and hope you have fun seeing what could-be.\nAll the best!\nRä"),
+      ),
+      appBar: AppBar(
+        title: const Text("Home"),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      backgroundColor: Colors.grey,
+    );
+  }
+}
+
+class AlarmsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Alarms"),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      backgroundColor: Colors.grey,
+    );
+  }
+}
+
+class SettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Settings"),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      backgroundColor: Colors.grey,
     );
   }
 }
